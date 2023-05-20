ed5a2dc0b69d86fea819025f827d859c9c171252
sunshine <sunshineboy085@gmail.com>
2023-05-21 00:28:06 +0800

    add:setting

--- src/views/Setting.vue
+++ src/views/Setting.vue
@@ -1,7 +1,67 @@
 <template>
-    <div class="container">123</div>
+    <div class="setting">
+        <div class="title">
+            用户
+        </div>
+
+        <div class="personal">
+            <div class="person">
+                <div class="image">
+                    <img width="100"
+                        src="https://codefun-proj-user-res-1256085488.cos.ap-guangzhou.myqcloud.com/645a6ddf5a7e3f0310fb6153/645e45b854fe000011615674/16845969771744096799.png">
+                </div>
+                <div class="pro">
+                    <div class="text">Pro</div>
+                </div>
+            </div>
+
+            <div class="name">John Smith</div>
+            <div class="email">johnsmith@gmail.com</div>
+        </div>
+
+        <div class="settingItem">
+            <div class="title">
+                设置
+            </div>
+
+            <div class="item">
+                <div class="icon">
+                    <img width="45"
+                        src="https://codefun-proj-user-res-1256085488.cos.ap-guangzhou.myqcloud.com/645a6ddf5a7e3f0310fb6153/645e45b854fe000011615674/ca34e626ac22be7fdc7c50d1e8421607.png">
+                </div>
+                <div class="itemText">
+                    <div class="itemTitle">Achievements</div>
+                    <div class="introduce">You have received 10 new badges this month.</div>
+                </div>
+            </div>
+
+            <div class="item">
+                <div class="icon">
+                    <img width="45"
+                        src="https://codefun-proj-user-res-1256085488.cos.ap-guangzhou.myqcloud.com/645a6ddf5a7e3f0310fb6153/645e45b854fe000011615674/2e42bfcc3370f198bf8a1dc54389d705.png">
+                </div>
+                <div class="itemText">
+                    <div class="itemTitle">Notifications</div>
+                    <div class="introduce">Manage the way we send you all the notifications and alerts.</div>
+                </div>
+            </div>
+
+            <div class="item">
+                <div class="icon">
+                    <img width="45"
+                        src="https://codefun-proj-user-res-1256085488.cos.ap-guangzhou.myqcloud.com/645a6ddf5a7e3f0310fb6153/645e45b854fe000011615674/37ff2b36fc4e59dacf32428172b42c0e.png">
+                </div>
+                <div class="itemText">
+                    <div class="itemTitle">Payment Details</div>
+                    <div class="introduce">You have 1 card ending 6789 linked to your account.</div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style style="scss" scoped>
+@import url(@/style/views/Setting.scss);
+</style>
