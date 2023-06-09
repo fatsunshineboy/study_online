dd5110080ed34db487add49b210773c747a56c07
fatsunshineboy <96576004+fatsunshineboy@users.noreply.github.com>
2023-06-09 14:46:57 +0000

    Deploying to gh-pages from @ fatsunshineboy/study_online@5c4705053970e28afb87622adae5081f807b0536 🚀

--- assets/cookie-37e58e91.js
+++
@@ -1,1 +0,0 @@
-import{y as t,r}from"./index-031436fb.js";const a=t("cookie",()=>{const o=r(localStorage.getItem("cookie")||"");return{cookie:o,setCookie:e=>{o.value=e,localStorage.setItem("cookie",e)},removeCookie:()=>{o.value="",localStorage.removeItem("cookie")}}});export{a as u};
